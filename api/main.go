af28aee71c7470e684b23fbba52cb029ca4cfc26
daikideal <54879050+daikideal@users.noreply.github.com>
2020-12-23 01:42:37 +0900

    Add: Goサーバーにブラウザからアクセスできるよう設定を追加
    - main.goでgoサーバーを立ち上げ、メッセージを表示した
    - Goのコンテナをport:8080で開いた
    - Goのコンテナのデフォルトコマンドを設定した
    - docker-composeのservice:apiにホスト:コンテナのportsを追加した
    
    ブラウザでlocalhost:8080にアクセスすると{"message":"Hello, World!"}が表示される。

--- api/main.go
+++ api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message":"Hello World!"}`))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", hello)
+	fmt.Println("Server started.")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
