74ac8e0d87d0d78525ea138488aceffab72994c9
daikideal <54879050+daikideal@users.noreply.github.com>
2020-12-31 20:04:45 +0900

    Add: 記事一覧を返すエンドポイントを追加
    デモデータでArticle(記事)を作成し、それらを全て返すようにした。
    jsonでデータをやりとりするようにパッケージを追加している。

--- api/main.go
+++ api/main.go
@@ -4,7 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Article struct {
+	Id int `json:"id"`
+	Title	string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:content`
+}
+
+var Articles []Article
+
+func createDemoArticles() {
+	Articles = []Article{
+		Article{Id: 1, Title: "Test Article", Desc: "Article Description", Content: "Test Content"},
+		Article{Id: 2, Title: "Test Article 2", Desc: "Article Description 2", Content: "Test Content 2"},
+	}
+}
+
+func returnArticles(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Articles)
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -14,10 +35,12 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/articles", returnArticles)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
 	fmt.Println("Server started.")
+	createDemoArticles()
 	handleRequests()
 }
